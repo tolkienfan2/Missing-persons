8055b6172b1a3bd0b1c0b3652994e1a82a6168da
tolkienfan2 <minni.ang@gmail.com>
2016-07-10 18:37:50 -0400

    Initial Commit

---
+++ Missing-Persons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Missing-Persons
+//
+//  Created by Minni K Ang on 2016-07-10.
+//  Copyright © 2016 CreativeIce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
